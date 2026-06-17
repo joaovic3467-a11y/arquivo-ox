dd1f298b861cee53ffddd0c04f5eb901c6b4d326
joaovic3467-a11y <joaovic3467@gmail.com>
2026-06-16 22:22:45 -0300

    Refactor main function and update validation tests
    Removed invalid test cases and added function declaration.

--- test_arquivo-ox.c
+++ test_arquivo-ox.c
@@ -1,19 +1,15 @@
 #include <assert.h>
+#include "function_skibidi.c"
+
+int validar(char c);
 
 int main()
 {
-    /* Casos válidos */
     assert(validar('X') == 1);
     assert(validar('O') == 1);
 
-    /* Casos inválidos */
     assert(validar('A') == 0);
     assert(validar('x') == 0);
-    assert(validar('o') == 0);
-    assert(validar('1') == 0);
-    assert(validar(' ') == 0);
-    assert(validar('\n') == 0);
-    assert(validar('*') == 0);
 
     return 0;
 }
