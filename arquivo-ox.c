8724f306290060eb33fa6dfa3fcdca17793d50d0
joaovic3467-a11y <joaovic3467@gmail.com>
2026-06-16 22:21:29 -0300

    Update arquivo-ox.c

--- arquivo-ox.c
+++ arquivo-ox.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "function_skibidi.c"
 
 int validar(char c);
-void gravar(char matriz[3][3], char nomeArquivo[]);
+int gravar(char matriz[3][3], char skibidiArquivo[]);
 
 int main(int argc, char *argv[])
 {
@@ -10,19 +11,19 @@ int main(int argc, char *argv[])
     int i, j;
     int k = 2;
 
-    if (argc != 11)
+    if(argc != 11)
     {
         printf("Uso: %s arquivo.txt O O O X X X O O O\n", argv[0]);
         return 1;
     }
 
-    for (i = 0; i < 3; i++)
+    for(i = 0; i < 3; i++)
     {
-        for (j = 0; j < 3; j++)
+        for(j = 0; j < 3; j++)
         {
             matriz[i][j] = argv[k][0];
 
-            if (!validar(matriz[i][j]))
+            if(!validar(matriz[i][j]))
             {
                 printf("Erro: use apenas X ou O\n");
                 return 1;
@@ -35,34 +36,4 @@ int main(int argc, char *argv[])
     gravar(matriz, argv[1]);
 
     return 0;
-}
-
-int validar(char c)
-{
-    return (c == 'X' || c == 'O');
-}
-
-void gravar(char matriz[3][3], char nomeArquivo[])
-{
-    FILE *arquivo;
-    int i, j;
-
-    arquivo = fopen(nomeArquivo, "w");
-
-    if (arquivo == NULL)
-    {
-        printf("Erro ao abrir o arquivo.\n");
-        return;
-    }
-
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            fprintf(arquivo, "%c ", matriz[i][j]);
-        }
-        fprintf(arquivo, "\n");
-    }
-
-    fclose(arquivo);
 }
