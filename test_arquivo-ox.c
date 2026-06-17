8724461afcb21b3735ac9a98580f1af2f4a9b9fb
joaovic3467-a11y <joaovic3467@gmail.com>
2026-06-16 22:01:58 -0300

    Update test_arquivo-ox.c

--- test_arquivo-ox.c
+++ test_arquivo-ox.c
@@ -4,11 +4,18 @@ int validar(char c);
 
 int main()
 {
+    /* Casos válidos */
     assert(validar('X') == 1);
     assert(validar('O') == 1);
 
+    /* Casos inválidos */
     assert(validar('A') == 0);
     assert(validar('x') == 0);
+    assert(validar('o') == 0);
+    assert(validar('1') == 0);
+    assert(validar(' ') == 0);
+    assert(validar('\n') == 0);
+    assert(validar('*') == 0);
 
     return 0;
 }
